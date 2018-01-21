ceb877a764cef76194a6225e4c63e0afe48f1cba
Agnieszka Wasikowska <aga@mcha.eu>
2018-01-21 10:52:52 +0000

    Image recognition works

--- android/app/src/main/java/com/mayank/aubergine/MainActivity.kt
+++ android/app/src/main/java/com/mayank/aubergine/MainActivity.kt
@@ -5,30 +5,16 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.content.pm.PackageManager
-import android.support.annotation.NonNull
-import android.os.AsyncTask.execute
-import android.os.Environment.getExternalStorageDirectory
 import android.graphics.Bitmap
 import android.os.Environment
 import android.view.View
 import android.widget.Button
-import android.content.ContentValues.TAG
-import android.os.AsyncTask
 import android.util.Log
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.getAs
 import java.io.*
-import java.net.HttpURLConnection
-import java.net.URL
-import android.widget.EditText
-import android.R.attr.button
 import android.annotation.SuppressLint
-import android.net.Uri
 import android.util.Base64
-import kotlin.experimental.and
-import android.graphics.BitmapFactory
-import android.support.v4.content.FileProvider
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -46,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         val permissions = arrayOf("android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE")
         requestPermissions(permissions, 0)
 
-        val cap: Button = findViewById(R.id.capture)
+        val cap: Button = findViewById(R.id.capture);
         cap.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View) {
                 // Code here executes on main thread after user presses button
@@ -54,7 +40,7 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        val scan: Button = findViewById(R.id.scan)
+        val scan: Button = findViewById(R.id.scan);
         scan.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View) {
                 // Code here executes on main thread after user presses button
@@ -103,73 +89,14 @@ class MainActivity : AppCompatActivity() {
         return image
     }
 
-    private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            var photoFile: File? = null
-            try {
-                photoFile = createImageFile();
-            } catch (ex: IOException) {
-                Log.e("balls", "createimage doesn't help")
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                val photoURI = FileProvider.getUriForFile(this,
-                "com.mayank.aubergine.fileprovider",
-                photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, CAP_CODE);
-            }
-        }
-    }
-
     fun captureTime(){
-//        Log.d("captureTime", "niceTime")
-//        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//
-//        if (intent.resolveActivity(getPackageManager()) != null) {
-//            Log.d("captureTime", "1")
-//
-//            // Create the File where the photo should go
-//            var photoFile: File? = null
-//            try {
-//                photoFile = createImageFile();
-//            } catch (ex: IOException) {
-//                Log.e("Error", ex.toString())
-//            }
-//
-//            Log.d("captureTime", "2")
-//        }
-        /*val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-        val imageFile = File(getExternalFilesDir(null), "image.jpg")
-        if (imageFile != null) {
-            val photoURI = FileProvider.getUriForFile(this,
-                    "com.mayank.aubergine.fileprovider",
-                    imageFile)
-            if (photoURI != null) {
-                Log.d("photoURI", "photoURI not null")
-            } else {
-                Log.d("photoURI", "photoURI null")
-            }
-            Log.d("captureTime", "3")
-
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-            startActivityForResult(intent, CAP_CODE)
-        }
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
         startActivityForResult(intent, CAP_CODE)
-        */
-        dispatchTakePictureIntent()
     }
 
     fun scanTime() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-        val imageFileUri = File(getExternalFilesDir(null), "image.jpg").toURI()
-        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageFileUri)
 
         startActivityForResult(intent, SCAN_CODE)
     }
@@ -178,48 +105,27 @@ class MainActivity : AppCompatActivity() {
         Log.d("onActivityResult", "1")
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(resultCode != RESULT_CANCELED && data != null && resultCode == RESULT_OK && resultCode != null) {
+        if (requestCode == CAP_CODE) {
+            val thumbnail = data.extras!!.get("data") as Bitmap
+            val bytes = ByteArrayOutputStream()
+            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
 
-            if (requestCode == CAP_CODE) {
-                val file = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString() + "/image.jpeg")
-                val size = file.length().toInt()
-                val bytes = ByteArray(size)
-                try {
-                    val buf = BufferedInputStream(FileInputStream(file))
-                    buf.read(bytes, 0, bytes.size)
-                    buf.close()
-                } catch (e: FileNotFoundException) {
-                    e.printStackTrace()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
+            uploadFileToServerTask(bytes, "createnote")
+        } else if (requestCode == SCAN_CODE) {
+            val thumbnail = data.extras!!.get("data") as Bitmap
+            val bytes = ByteArrayOutputStream()
+            thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
 
-                uploadFileToServerTask(bytes, "createnote")
-            } else if (requestCode == SCAN_CODE) {
-                val file = File(getExternalFilesDir(null).toString() + "/image.jpeg")
-                val size = file.length().toInt()
-                val bytes = ByteArray(size)
-                try {
-                    val buf = BufferedInputStream(FileInputStream(file))
-                    buf.read(bytes, 0, bytes.size)
-                    buf.close()
-                } catch (e: FileNotFoundException) {
-                    e.printStackTrace()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-
-                uploadFileToServerTask(bytes, "findnote")
-            }
+            uploadFileToServerTask(bytes, "findnote")
         }
     }
 
-    fun uploadFileToServerTask(bytes: ByteArray, endpoint: String) {
-        Log.d("Size of image in B", bytes.size.toString())
+    fun uploadFileToServerTask(bytes: ByteArrayOutputStream, endpoint: String) {
+        Log.d("Size of image in B", bytes.toByteArray().size.toString())
 
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         // 8 is URL safe
-        val b64str = Base64.encodeToString(bytes, 8)
+        val b64str = Base64.encodeToString(bytes.toByteArray(), 8)
         Log.d("Base64Conv", b64str)
 
         Fuel.post("http://52.174.181.163:5001/$endpoint").body("image=$b64str&name=$timeStamp").response { request, response, result ->
@@ -241,6 +147,8 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
     }
+
 }
 
